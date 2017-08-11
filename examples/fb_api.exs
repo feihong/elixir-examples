1709ef87eca1997c9860ebb89ee1f41ef664afe2
Feihong Hsu <feihong.hsu@gmail.com>
2017-08-10 21:41:50 -0500

    Print the names of all downloaded events

--- examples/fb_api.exs
+++ examples/fb_api.exs
@@ -9,9 +9,17 @@ data = HTTPoison.get!(
   [],
   params: %{access_token: access_token})
   |> (fn response -> response.body end).()
+  |> Poison.decode!
+
+# IO.puts data
 
 # Write the prettified JSON to file.
 data
-  |> Poison.decode!
   |> Poison.encode!(pretty: true)
   |> (fn data -> File.write("response.json", data) end).()
+
+events = data["data"]
+for event <- events do
+  IO.puts event["name"]
+end
+IO.puts "Wrote #{length(events)} items to response.json"
