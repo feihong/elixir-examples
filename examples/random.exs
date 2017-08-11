77ad4ebcb127efccfe6d93e7c65470d1709a0ee2
Feihong Hsu <feihong.hsu@gmail.com>
2017-08-10 21:56:56 -0500

    Add an example of using Enum.random/1

---
+++ examples/random.exs
@@ -0,0 +1,14 @@
+greetings = """
+Hello World
+Hallo Welt
+你好世界
+こんにちは世界
+Saluton mondo
+"""
+|> String.trim()
+|> String.split("\n")
+
+# IO.inspect greetings
+for _ <- 1..10 do
+  IO.puts Enum.random(greetings)
+end
