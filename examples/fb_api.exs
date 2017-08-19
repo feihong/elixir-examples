1c1512b7f572335b8df4a8aa5bbd5d3611299b01
Feihong Hsu <feihong.hsu@gmail.com>
2017-08-19 00:11:19 -0500

    Print events in chronological order

--- examples/fb_api.exs
+++ examples/fb_api.exs
@@ -5,33 +5,45 @@ defmodule Fetch do
   @access_token config["access_token"]
 
   def fetch(name) do
-    params = %{access_token: @access_token,
-               since: DateTime.utc_now() |> DateTime.to_iso8601()}
-    data = HTTPoison.get!(
-      "https://graph.facebook.com/v2.9/#{name}/events/",
-      [],
-      params: params)
-      |> (fn response -> response.body end).()
-      |> Poison.decode!
+    IO.puts name
+    path = "cache_pages/facebook__#{name}.json"
 
+    if File.exists?(path) do
+      File.read!(path)
+      |> Poison.decode!
+      |> (fn data -> data["data"] end).()
+    else
+      params = %{access_token: @access_token,
+                 since: DateTime.utc_now |> DateTime.to_iso8601}
+      data = HTTPoison.get!(
+        "https://graph.facebook.com/v2.9/#{name}/events/",
+        [],
+        params: params)
+        |> (fn response -> response.body end).()
+        |> Poison.decode!
+
+      write_to_file(path, data)
+      data["data"]
+    end
+  end
+
+  def write_to_file(path, data) do
     data
+      |> Poison.encode!(pretty: true)
+      |> (fn text -> File.write(path, text) end).()
   end
 end
 
 
 pages = config["pages"]
 
-data = Fetch.fetch(List.first pages)
-
-# Write the prettified JSON to file.
-data
-  |> Poison.encode!(pretty: true)
-  |> (fn data -> File.write("response.json", data) end).()
-
-events = data["data"]
-|> Enum.sort_by(fn x -> x["start_time"] end)
+events = pages
+  |> Enum.map(fn name -> Fetch.fetch(name) end)
+  |> List.flatten
+  |> Enum.sort_by(fn event -> event["start_time"] end)
 
 for event <- events do
-  IO.puts "#{event["name"]}, #{event["start_time"]}"
+  IO.puts event["name"]
+  IO.puts event["start_time"]
+  IO.puts "------------------------"
 end
-IO.puts "\nWrote #{length(events)} items to response.json"
