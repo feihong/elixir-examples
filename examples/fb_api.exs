237b561b4019e8cbdca1186504216b72a0ed4507
Feihong Hsu <feihong.hsu@gmail.com>
2017-08-20 15:34:50 -0500

    Add new Download module that handles HTTP requests and caching

--- examples/fb_api.exs
+++ examples/fb_api.exs
@@ -1,36 +1,46 @@
 config = File.read!("config.json")
   |> Poison.decode!
 
+
+defmodule Download do
+  def fetch(cache_name, url, params) do
+    path = "cache_pages/#{cache_name}.json"
+
+    if File.exists?(path) do
+      File.read!(path)
+      |> Poison.decode!
+    else
+      data = HTTPoison.get!(url, [], params: params)
+        |> (fn response -> response.body end).()
+        |> Poison.decode!
+
+      write_to_file(path, data)
+      data
+    end
+  end
+
+  defp file_is_recent?(path) do
+    File.exists?(path)
+  end
+
+  defp write_to_file(path, data) do
+    data
+      |> Poison.encode!(pretty: true)
+      |> (fn text -> File.write(path, text) end).()
+  end
+end
+
 defmodule Fetch do
   @access_token config["access_token"]
 
   def fetch(name) do
     IO.puts name
-    path = "cache_pages/facebook__#{name}.json"
+    cache_name = "facebook__#{name}"
+    url = "https://graph.facebook.com/v2.9/#{name}/events/"
+    params = %{access_token: @access_token,
+               since: DateTime.utc_now |> DateTime.to_iso8601}
 
-    if File.exists?(path) do
-      File.read!(path)
-      |> Poison.decode!
-      |> (fn data -> data["data"] end).()
-    else
-      params = %{access_token: @access_token,
-                 since: DateTime.utc_now |> DateTime.to_iso8601}
-      data = HTTPoison.get!(
-        "https://graph.facebook.com/v2.9/#{name}/events/",
-        [],
-        params: params)
-        |> (fn response -> response.body end).()
-        |> Poison.decode!
-
-      write_to_file(path, data)
-      data["data"]
-    end
-  end
-
-  def write_to_file(path, data) do
-    data
-      |> Poison.encode!(pretty: true)
-      |> (fn text -> File.write(path, text) end).()
+    Download.fetch(cache_name, url, params)["data"]
   end
 end
 
