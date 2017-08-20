479b4ff8e395f78e6e14a176ba3bac2f9f09f6d3
Feihong Hsu <feihong.hsu@gmail.com>
2017-08-20 16:35:52 -0500

    Don't download data for cached data newer than 24 hours

--- examples/fb_api.exs
+++ examples/fb_api.exs
@@ -1,15 +1,22 @@
+require Logger
+
+
 config = File.read!("config.json")
   |> Poison.decode!
 
 
 defmodule Download do
+  @hour 60 * 60
+
   def fetch(cache_name, url, params) do
     path = "cache_pages/#{cache_name}.json"
 
-    if File.exists?(path) do
+    if file_is_recent?(path) do
+      Logger.info "Retrieving #{cache_name} from cache"
       File.read!(path)
       |> Poison.decode!
     else
+      Logger.info "Downloading #{cache_name} from #{url}"
       data = HTTPoison.get!(url, [], params: params)
         |> (fn response -> response.body end).()
         |> Poison.decode!
@@ -19,8 +26,12 @@ defmodule Download do
     end
   end
 
+
+  # Returns true if the file at the given path exists and was created less than
+  # 24 hours ago; false otherwise.
   defp file_is_recent?(path) do
-    File.exists?(path)
+    ctime = File.stat!(path, time: :posix).ctime |> DateTime.from_unix!
+    File.exists?(path) and DateTime.diff(DateTime.utc_now(), ctime) < (24 * @hour)
   end
 
   defp write_to_file(path, data) do
@@ -34,7 +45,6 @@ defmodule Fetch do
   @access_token config["access_token"]
 
   def fetch(name) do
-    IO.puts name
     cache_name = "facebook__#{name}"
     url = "https://graph.facebook.com/v2.9/#{name}/events/"
     params = %{access_token: @access_token,
@@ -57,3 +67,5 @@ for event <- events do
   IO.puts event["start_time"]
   IO.puts "------------------------"
 end
+
+IO.puts "Fetched #{length(events)} events"
