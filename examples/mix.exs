7cffb3563aef5009d86db0af906c9fc7cab524c1
Feihong Hsu <feihong.hsu@gmail.com>
2017-08-20 21:39:05 -0500

    Use human readable format for date and time

--- examples/mix.exs
+++ examples/mix.exs
@@ -14,7 +14,7 @@ defmodule Examples.Mixfile do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      applications: [:httpoison],
+      applications: [:httpoison, :timex],
       extra_applications: [:logger]
     ]
   end
@@ -25,7 +25,8 @@ defmodule Examples.Mixfile do
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
       {:poison, "~> 3.1"},
       {:httpoison, "~> 0.12"},
-      {:slime, "~> 1.0.0"}
+      {:slime, "~> 1.0.0"},
+      {:timex, "~> 3.1"}
     ]
   end
 end
