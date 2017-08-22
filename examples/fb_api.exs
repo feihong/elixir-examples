1a189932b579d7ebd7a31b2c9b04888ce95005b8
Feihong Hsu <feihong.hsu@gmail.com>
2017-08-22 17:16:58 -0500

    Sort by keyword match as well as start time

--- examples/fb_api.exs
+++ examples/fb_api.exs
@@ -1,8 +1,20 @@
 
 defmodule Fetch do
+  @keywords Application.fetch_env!(:examples, :keywords)
   @access_token Application.fetch_env!(:examples, Facebook)[:access_token]
 
-  def fetch(name) do
+  def fetch_all() do
+    pages = Application.fetch_env!(:examples, Facebook)[:pages]
+
+    # Fetch all events and sort by start_time.
+    events = pages
+      |> Enum.map(fn name -> fetch(name) end)
+      |> List.flatten
+      |> Enum.map(&add_fields/1)
+      |> Enum.sort_by(&sort_mapper/1)
+  end
+
+  defp fetch(name) do
     cache_name = "facebook__#{name}"
     url = "https://graph.facebook.com/v2.9/#{name}/events/"
     params = %{access_token: @access_token,
@@ -11,22 +23,25 @@ defmodule Fetch do
     Download.fetch(cache_name, url, params)["data"]
   end
 
-  def add_url(evt) do
-    Map.put evt, "url", "https://facebook.com/events/#{evt["id"]}"
+  defp add_fields(evt) do
+    text = evt["name"] <> " " <> evt["description"] |> String.downcase
+    keyword_match = Enum.any?(@keywords,
+      fn keyword -> String.contains?(text, keyword) end)
+
+    evt
+      |> Map.put("url", "https://facebook.com/events/#{evt["id"]}")
+      |> Map.put("keyword_match", keyword_match)
+  end
+
+  defp sort_mapper(evt) do
+    # Make sure that events that have desired keywords are always in front
+    # regardless of start time.
+    num = if evt["keyword_match"], do: 0, else: 1
+    {num, evt["start_time"]}
   end
 end
 
 
-pages = Application.fetch_env!(:examples, Facebook)[:pages]
-
-# Fetch all events and sort by start_time.
-events = pages
-  |> Enum.map(fn name -> Fetch.fetch(name) end)
-  |> List.flatten
-  |> Enum.map(&Fetch.add_url/1)
-  |> Enum.sort_by(fn event -> event["start_time"] end)
-
-
-input = "report.slime" |> File.read!
-Slime.render(input, events: events)
+template = "report.slime" |> File.read!
+Slime.render(template, events: Fetch.fetch_all())
   |> (fn output -> File.write("report.html", output) end).()
