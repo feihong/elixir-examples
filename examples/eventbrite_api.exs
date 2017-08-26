1673d9d337a6becb056b88820c542f74f842217c
Feihong Hsu <feihong.hsu@gmail.com>
2017-08-26 12:33:14 -0500

    Download and display venue info

--- examples/eventbrite_api.exs
+++ examples/eventbrite_api.exs
@@ -1,19 +1,31 @@
 defmodule EventBrite do
+  @base_url "https://www.eventbriteapi.com/v3"
   @access_token Application.fetch_env!(:examples, EventBrite)[:access_token]
 
   def fetch_all() do
-    url = "https://www.eventbriteapi.com/v3/events/search/"
+    url = "#{@base_url}/events/search/"
     params = %{
       token: @access_token,
       q: "china",
       "location.address": "Chicago, IL"
     }
     Download.fetch("eventbrite__china", url, params)["events"]
+      |> Enum.map(&download_venue/1)
+  end
+
+  def download_venue(evt) do
+    # IO.inspect evt
+    venue_id = evt["venue_id"]
+    url = "#{@base_url}/venues/#{venue_id}/"
+    params = %{token: @access_token}
+    venue = Download.fetch("eventbrite__venue__#{venue_id}", url, params)
+    evt |> Map.put("venue", venue)
   end
 end
 
-for event <- EventBrite.fetch_all() do
-  IO.puts event["name"]["text"]
-  IO.puts event["start"]["local"]
+for evt <- EventBrite.fetch_all() do
+  IO.puts evt["name"]["text"]
+  IO.puts evt["venue"]["name"]
+  IO.puts evt["start"]["local"]
   IO.puts ""
 end
