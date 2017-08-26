67cf1bfbdcba127cefe21ced6e9a9bc15a6f9ff8
Feihong Hsu <feihong.hsu@gmail.com>
2017-08-26 12:23:22 -0500

    Create EventBrite module

--- examples/eventbrite_api.exs
+++ examples/eventbrite_api.exs
@@ -1,13 +1,18 @@
-url = "https://www.eventbriteapi.com/v3/events/search/"
-token = Application.fetch_env!(:examples, EventBrite)[:access_token]
-params = %{
-  token: token,
-  q: "china",
-  "location.address": "Chicago, IL"
-}
-# IO.inspect params
-events = Download.fetch("eventbrite__china", url, params)["events"]
-for event <- events do
+defmodule EventBrite do
+  @access_token Application.fetch_env!(:examples, EventBrite)[:access_token]
+
+  def fetch_all() do
+    url = "https://www.eventbriteapi.com/v3/events/search/"
+    params = %{
+      token: @access_token,
+      q: "china",
+      "location.address": "Chicago, IL"
+    }
+    Download.fetch("eventbrite__china", url, params)["events"]
+  end
+end
+
+for event <- EventBrite.fetch_all() do
   IO.puts event["name"]["text"]
   IO.puts event["start"]["local"]
   IO.puts ""
