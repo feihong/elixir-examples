b38ed252e3f7b45ba57b689e51c9f8f7d3710907
Feihong Hsu <feihong.hsu@gmail.com>
2017-08-26 12:17:28 -0500

    Search for events using EventBrite API

--- examples/eventbrite_api.exs
+++ examples/eventbrite_api.exs
@@ -1,5 +1,14 @@
-url = "https://www.eventbriteapi.com/v3/users/me/"
+url = "https://www.eventbriteapi.com/v3/events/search/"
 token = Application.fetch_env!(:examples, EventBrite)[:access_token]
-params = %{token: token}
-
-Download.fetch("eventbrite__me", url, params)
+params = %{
+  token: token,
+  q: "china",
+  "location.address": "Chicago, IL"
+}
+# IO.inspect params
+events = Download.fetch("eventbrite__china", url, params)["events"]
+for event <- events do
+  IO.puts event["name"]["text"]
+  IO.puts event["start"]["local"]
+  IO.puts ""
+end
