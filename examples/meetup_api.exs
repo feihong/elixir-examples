e4cd09dae14f5f32d29dd40efc55d88e0b87da2f
Feihong Hsu <feihong.hsu@gmail.com>
2017-08-27 01:07:56 -0500

    Make Meetup example more realistic

--- examples/meetup_api.exs
+++ examples/meetup_api.exs
@@ -1,8 +1,22 @@
 url = "https://api.meetup.com/find/venues"
 params = %{
   key: Application.fetch_env!(:examples, Meetup)[:api_key],
-  location: "Chicago, IL",
-  text: "Whitney M. Young Magnet High School",
-  zip: "60607"
+  location: "Millenium Park",
+  text:  "Millenium Park",
+  longitude: "-87.62185999999997",
+  latitude: "41.8831824",
+  radius: "1"
 }
-Download.fetch("meetup__venue", url, params)
+
+venues = Download.fetch("meetup__venue", url, params)
+for venue <- venues do
+  IO.puts venue["name"]
+end
+
+IO.puts "Got #{length(venues)} venues"
+
+# data = HTTPoison.get!(url, [], params: params)
+#   |> (fn response -> response.body end).()
+#   |> Poison.decode!
+
+# IO.puts(data |> Poison.encode!(pretty: true))
