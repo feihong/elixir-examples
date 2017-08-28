2dc93a4fd4f7f21552e731b50be84f44782e6bd7
Feihong Hsu <feihong.hsu@gmail.com>
2017-08-27 22:20:57 -0500

    Use explore instead of search

--- examples/foursquare_venue_search.exs
+++ examples/foursquare_venue_search.exs
@@ -1,6 +1,6 @@
-# https://developer.foursquare.com/docs/venues/search
+# https://developer.foursquare.com/docs/venues/explore
 
-url = "https://api.foursquare.com/v2/venues/search"
+url = "https://api.foursquare.com/v2/venues/explore"
 cfg = Application.fetch_env!(:examples, FourSquare)
 params = %{
   v: "20170827",
@@ -9,18 +9,25 @@ params = %{
   client_secret: cfg[:client_secret],
   ll: "41.854107,-87.632086",
   radius: 1600,
-  query: "restaurant",
-  intent: "browse",
+  section: "food",
   limit: 50,
 }
 
-venues = Download.fetch("foursquare__venues", url, params)["response"]["venues"]
-for {venue, num} <- Enum.with_index(venues, 1) do
+groups = Download.fetch("foursquare__venues", url, params)["response"]["groups"]
+
+group_names = groups |> Enum.map(fn gr -> gr["name"] end)
+IO.puts "Groups: #{inspect group_names}"
+
+
+items = List.first(groups)["items"]
+for {item, num} <- Enum.with_index(items, 1) do
+  # IO.inspect item
+  venue = item["venue"]
   IO.puts "#{num}. #{venue["name"]}"
   categories = venue["categories"] |> Enum.map(fn cat -> cat["shortName"] end)
   IO.puts "    " <> Enum.join(categories, ", ")
+  IO.puts "    " <> venue["createdAt"]
   IO.puts ""
 end
 
-
-IO.puts "Got #{length(venues)} venues"
+IO.puts "Got #{length(items)} venues"
