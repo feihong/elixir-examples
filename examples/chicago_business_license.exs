efffb111161a7b75aa8d849a07b0053f68a57251
Feihong Hsu <feihong.hsu@gmail.com>
2017-08-29 20:31:26 -0500

    Add useful comments

--- examples/chicago_business_license.exs
+++ examples/chicago_business_license.exs
@@ -1,8 +1,11 @@
-# https://developer.foursquare.com/docs/venues/explore
+# Browse data:
+# https://data.cityofchicago.org/Community-Economic-Development/Business-Licenses-Current-Active/uupf-x98q
+# API docs:
+# https://dev.socrata.com/foundry/data.cityofchicago.org/xqx5-8hwx
 
 url = "https://data.cityofchicago.org/resource/xqx5-8hwx.json"
 app_token = Application.fetch_env!(:examples, BusinessLicense)[:app_token]
-date = Timex.shift(Timex.today(), days: -30)
+date = Timex.shift(Timex.today(), months: -6)
 date_str = Timex.format!(date, "{YYYY}-{0M}-{0D}T00:00:00.000")
 params = %{
   "$$app_token": app_token,
