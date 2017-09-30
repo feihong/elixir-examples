f4b9ed3a400f6f4b5078f4e48b250704949f73a4
Feihong Hsu <feihong.hsu@gmail.com>
2017-09-30 12:20:59 -0500

    Fixed script that finds meet up venues

--- examples/meetup_find_venue.exs
+++ examples/meetup_find_venue.exs
@@ -1,22 +1,27 @@
+name = "AMC River East 21"
+
 url = "https://api.meetup.com/find/venues"
 params = %{
   key: Application.fetch_env!(:examples, Meetup)[:api_key],
-  location: "Millenium Park",
-  text:  "Millenium Park",
-  longitude: "-87.62185999999997",
-  latitude: "41.8831824",
-  radius: "1"
+  text: name,
+  location: "322 E Illinois St, Chicago, IL 60611",
 }
 
 venues = Download.fetch("meetup__venue", url, params)
-for venue <- venues do
-  IO.puts venue["name"]
-end
+match_venues =
+  venues
+  |> Enum.filter(fn v -> v["name"] == name end)
+  |> Enum.sort_by(fn v -> v["rating_count"] end, &>=/2)
 
-IO.puts "Got #{length(venues)} venues"
+# for venue <- match_venues do
+#   IO.puts "# ratings: #{venue["rating_count"]}, rating: #{venue["rating"]}"
+#   IO.puts venue["address_1"]
+# end
 
-# data = HTTPoison.get!(url, [], params: params)
-#   |> (fn response -> response.body end).()
-#   |> Poison.decode!
+venue = List.first(match_venues)
+IO.puts "Most rated venue:"
+IO.puts "Rating: #{venue["rating"]}"
+IO.puts "Rating count: #{venue["rating_count"]}"
+IO.puts "Address: #{venue["address_1"]}"
 
-# IO.puts(data |> Poison.encode!(pretty: true))
+IO.puts "\nGot #{length(venues)} venues, #{length(match_venues)} that matched"
