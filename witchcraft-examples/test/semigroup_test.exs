5a09a989da58d2c3b5b9f9bc2c8c457b4866c9bb
Feihong Hsu <feihong.hsu@gmail.com>
2017-10-24 22:49:59 -0500

    Add more semigroup examples

--- witchcraft-examples/test/semigroup_test.exs
+++ witchcraft-examples/test/semigroup_test.exs
@@ -1,3 +1,5 @@
+# https://github.com/expede/witchcraft/blob/master/lib/witchcraft/semigroup.ex
+
 defmodule SemigroupTest do
   use ExUnit.Case
   use Witchcraft.Semigroup
@@ -20,5 +22,25 @@ defmodule SemigroupTest do
     s1 = [1,2,3] |> Enum.into(MapSet.new)
     s2 = Enum.into([2,3,4,5], MapSet.new)
     assert s1 <> s2 == [1,2,3,4,5] |> MapSet.new
+  end
+
+  test "function" do
+    fn1 = &(&1 + 1)
+    fn2 = &(&1 * 2)
+    assert (fn1 <> fn2).(4) == 10
+  end
+
+  test "concat" do
+    result = concat([
+      [1,2,3],
+      [4,5],
+      [6,7,8],
+      [9]
+    ])
+    assert result == Enum.to_list 1..9
+  end
+
+  test "repeat" do
+    assert repeat({"a", 3}, times: 3) == {"aaa", 9}
   end
 end
